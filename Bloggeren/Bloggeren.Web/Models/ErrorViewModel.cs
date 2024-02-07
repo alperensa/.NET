2e2d7110d0e371c7cbd7cd10c0cb3be7752d09ed
Alperen <alperensarierikli@hotmail.com>
2024-02-07 18:45:48 +0300

    init

---
+++ Bloggeren/Bloggeren.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Bloggeren.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
