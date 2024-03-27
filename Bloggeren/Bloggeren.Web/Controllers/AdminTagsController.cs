7c921f94b694166094ae90acfe30a48c8905cd75
Alperen <alperensarierikli@hotmail.com>
2024-03-27 14:42:46 +0300

    commit

---
+++ Bloggeren/Bloggeren.Web/Controllers/AdminTagsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bloggeren.Web.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult SubmitTag()
+        {
+            var name = Request.Form["name"];
+            var displayName = Request.Form["displayName"];
+            return View("Add");
+        }
+    }
+}
